1f2379b7219f4cc64830216a163617aed6b8500d
DominikDblk <dominik@deblock.com>
2025-08-02 11:47:21 +0200

    added calculateMonthlyDcaInEuro

---
+++ app/calculationV3/calculateMonthlyDcaInEuro.ts
@@ -0,0 +1,8 @@
+function calculateMonthlyDcaInEuro(
+  dcaBaseEuro: number,
+  cpiFactor: number,
+  enableIndexing: boolean
+) {
+  const dcaIndexed = enableIndexing ? dcaBaseEuro * cpiFactor : dcaBaseEuro;
+  return dcaIndexed;
+}
